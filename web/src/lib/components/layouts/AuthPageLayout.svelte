a815e388e82502d94527b68587a775020870440c
eelvinb <basubasee@gmail.com>
2026-05-01 04:28:50 +0800

    Update AuthPageLayout.svelte

--- web/src/lib/components/layouts/AuthPageLayout.svelte
+++ web/src/lib/components/layouts/AuthPageLayout.svelte
@@ -30,7 +30,7 @@
       <CardHeader class="mt-6">
         <VStack>
           <Logo variant="icon" size="giant" />
-          <Heading size="large" class="font-semibold" color="primary" tag="h1">Equal</Heading>
+          <Heading size="large" class="font-semibold" color="primary" tag="h1">{title}</Heading>
         </VStack>
       </CardHeader>
     {/if}
